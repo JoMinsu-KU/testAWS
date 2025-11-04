f91fd156087bd501ade817a7911407685783b4e9
root <root@ip-172-31-44-77.ap-northeast-2.compute.internal>
2025-11-04 03:08:34 +0000

    console메시지 수정

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get('/', function(req, res, next) {
   console.log("hello");
   //res.render('index', { title: 'Express' });
-  res.json("Node.js Framework Express Go 7");
+  res.json("Node.js Framework Express Go 25");
 });
 
 router.get('/healthcheck', function(req, res, next) {
